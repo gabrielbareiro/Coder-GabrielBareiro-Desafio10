478dba73f5546b31d13a8681970769d58afb8fbb
gabrielbareiro <minimalposadas@gmail.com>
2022-08-16 23:16:55 -0300

    subo ejs al repositorio

---
+++ ejs/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const {Container} = require("./container");
+
+app.use (express.urlencoded({extended: true}));
+
+const PORT = process.env.PORT || 8080;
+
+const container = new Container('./data/products.txt');
+
+app.set ('view engine', 'ejs');
+app.set ('views', "./views");
+
+app.use (express.static('public'));
+
+app.get ('/', async (req, res) => {
+    const products = await container.getAll()
+    res.render ('index', {
+        listExist: true,
+        list: products,
+        products: true
+    })
+});
+
+app.get ('/productos', async (req, res) =>{
+    const products = await container.getAll();
+    res.render ('partials/products', {
+        listExist: true,
+        list: products,
+        products: true
+    })
+});
+
+app.post ('/productos', async (req, res) => {
+    const products = req.body;
+    container.save(products);
+    res.redirect('/productos');
+});
+
+app.listen (PORT, err => {
+    if (err) throw new Error (`Error on server: ${err}`);
+    console.log (`Server is runnin on port: ${PORT}`);
+})
